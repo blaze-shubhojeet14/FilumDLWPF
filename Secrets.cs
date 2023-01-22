fc9be0643ddf8d60edc2585029d6dc0fec4ae04b
Shubhojeet Choudhury <shubhojeetblaze14@gmail.com>
2023-01-22 05:44:13 +0530

    v1.2.0 - Added Twitter as a Platform to download images and videos, made the code more compact using methods, and fixed majors bugs | First Major Update of 2023!

---
+++ Secrets.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FilumDLWPF
+{
+    public class Secrets
+    {
+        public string TwitterBearerToken { get; set; }
+        public string SetTwitterBearerToken()
+        {
+            TwitterBearerToken = "Bearer AAAAAAAAAAAAAAAAAAAAAE62lAEAAAAA2pDCyjRIzEnhAR%2B3iZwNPSJn8OM%3DnDuGSQkQ95qldcjTnriF6AM6btmdv2xnx7JY7D4eFB9DbaYO63";
+            return TwitterBearerToken;
+        }
+        
+    }
+}
